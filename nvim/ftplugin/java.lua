de239197da270e5223ed793642228390d6561f24
David Jacobs <david@wit.io>
2025-10-23 20:40:09 -0400

    Add Java completion for adding imports.

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,3 +1,10 @@
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 
+-- Java-specific LSP keybindings
+vim.keymap.set('n', '<Leader>oi', function()
+  vim.lsp.buf.code_action({
+    context = { only = { 'source.organizeImports' } },
+    apply = true
+  })
+end, { buffer = true, desc = 'Organize imports' })
