6da7ef855ba1910a2d5cd8fcaa65d055209f31a4
David Jacobs <david@wit.io>
2026-04-06 21:15:15 -0400

    Add bash, clojure, python, and typescript Treesitter parsers.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  lazy = false,
   config = function()
-    require("nvim-treesitter").setup({
-      ensure_installed = { "haskell", "javascript", "markdown", "markdown_inline", "ruby" },
-      highlight = { enable = true }
+    require("nvim-treesitter").install { "bash", "clojure", "haskell", "javascript", "markdown", "markdown_inline", "python", "ruby", "typescript" }
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "bash", "clojure", "haskell", "javascript", "markdown", "python", "ruby", "sh", "typescript" },
+      callback = function() vim.treesitter.start() end
     })
   end
 }
