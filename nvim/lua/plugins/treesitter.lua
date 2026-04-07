e647f3f04547b31c2faff26ea552b5d3be2d0f43
David Jacobs <david@wit.io>
2026-04-06 21:52:27 -0400

    Clean up Neovim config.
    - Remove vim.loop shim in lazy.lua since Neovim 0.12+ always has vim.uv
    - Reformat treesitter.lua language lists to one language per line
    - Remove commented out code for previous Nord theme

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,9 +3,43 @@ return {
   build = ":TSUpdate",
   lazy = false,
   config = function()
-    require("nvim-treesitter").install { "bash", "c", "clojure", "cpp", "css", "haskell", "html", "java", "javascript", "lua", "markdown", "markdown_inline", "python", "ruby", "typescript", "yaml" }
+    require("nvim-treesitter").install {
+      "bash",
+      "c",
+      "clojure",
+      "cpp",
+      "css",
+      "haskell",
+      "html",
+      "java",
+      "javascript",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "ruby",
+      "typescript",
+      "yaml"
+    }
     vim.api.nvim_create_autocmd("FileType", {
-      pattern = { "bash", "c", "clojure", "cpp", "css", "haskell", "html", "java", "javascript", "lua", "markdown", "python", "ruby", "sh", "typescript", "yaml" },
+      pattern = {
+        "bash",
+        "c",
+        "clojure",
+        "cpp",
+        "css",
+        "haskell",
+        "html",
+        "java",
+        "javascript",
+        "lua",
+        "markdown",
+        "python",
+        "ruby",
+        "sh",
+        "typescript",
+        "yaml"
+      },
       callback = function() vim.treesitter.start() end
     })
   end
