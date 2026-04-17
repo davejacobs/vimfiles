ac3527a82c3f63f36e1f82913377672e316b26da
David Jacobs <david@wit.io>
2026-04-16 20:54:35 -0400

    Clear RenderMarkdownCode background on colorscheme change.

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -2,8 +2,12 @@ return {
   "MeanderingProgrammer/render-markdown.nvim",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   ft = { "markdown" },
-  opts = {
-    indent = { enabled = false },
-    code = { enabled = false }
-  }
+  config = function()
+    require("render-markdown").setup({ indent = { enabled = false } })
+    local function set_hl()
+      vim.api.nvim_set_hl(0, "RenderMarkdownCode", { bg = "NONE" })
+    end
+    set_hl()
+    vim.api.nvim_create_autocmd("ColorScheme", { callback = set_hl })
+  end
 }
